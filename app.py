53da47c5d7c38a9f28c2b38a042a298beb7d9f66
Tyler <gdchoices43@github.com>
2021-02-18 14:56:04 -0800

    Updated app.py
    Getting the base for the project started.

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-from constants import TEAMS, PLAYERS,
+import constants
+import sys
+import copy
+import statistics
 
+players_copy = copy.deepcopy(constants.PLAYERS)
+teams_copy = copy.deepcopy(constants.TEAMS)
+
+
+def clean_data():
+    pass
+
+
+def balance_teams():
+    pass
 
 
 if __name__ == "__main__":
+    clean_data()
