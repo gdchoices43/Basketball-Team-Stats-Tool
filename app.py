4b201d34a146e712ccdfd858df2a7cc2577b8771
Tyler <gdchoices43@github.com>
2021-02-19 14:57:02 -0800

    more bug fixes
    Still not sure why I'm only getting the first 6  players on the list on every team i choose it's the same 6 players

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ no_experience = []
 
 
 def clean_data():
-    experience()
     balance_teams()
 
 
@@ -69,31 +68,33 @@ Enter-> B) To Exit
                 team_bandits = balance_teams()[0]
                 num_players = len(team_bandits)
                 print("\nTeam: Bandits\n=+=+=+=+=+=+=+=+=+=+=+=+=+\nPlayers: {}\n".format(num_players))
-                team_list1 = []
+                team_list_a = []
                 for player in team_bandits:
                     players_name = player["name"]
-                    team_list1.append(str(players_name))
-                    print(", ".join(team_list1))
+                    team_list_a.append(str(players_name))
+                    # Got this solution from "Daniel OSullavin". I was using .join and it was not the result
+                    # I wanted and for the life of me couldn't figure it out
+                    print(player["name"], end=", ")
                 break
             elif team_option.upper() == "B":
                 team_warriors = balance_teams()[0]
                 num_players = len(team_warriors)
                 print("\nTeam: Warriors\n=+=+=+=+=+=+=+=+=+=+=+=+=+\nPlayers: {}\n".format(num_players))
-                team_list2 = []
+                team_list_b = []
                 for player in team_warriors:
                     players_name = player["name"]
-                    team_list2.append(str(players_name))
-                    print(", ".join(team_list2))
+                    team_list_b.append(str(players_name))
+                    print(player["name"], end=", ")
                 break
             elif team_option.upper() == "C":
                 team_panthers = balance_teams()[0]
                 num_players = len(team_panthers)
                 print("\nTeam: Panthers\n=+=+=+=+=+=+=+=+=+=+=+=+=+\nPlayers: {}\n".format(num_players))
-                team_list3 = []
+                team_list_c = []
                 for player in team_panthers:
                     players_name = player["name"]
-                    team_list3.append(str(players_name))
-                    print(", ".join(team_list3))
+                    team_list_c.append(str(players_name))
+                    print(player["name"], end=", ")
                 break
             raise ValueError()
         except ValueError as e:
@@ -102,4 +103,5 @@ Enter-> B) To Exit
 
 
 if __name__ == "__main__":
+    clean_data()
     team_stat_menu()
