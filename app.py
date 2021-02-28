eae2dbeaa33c5184efcd5e7df0afa8e63bd7836c
Tyler Duvall <gdchoices43@gmail.com>
2021-02-28 14:38:51 -0800

    Finalized code
    Finished making the code the best I could.

--- app.py
+++ app.py
@@ -6,35 +6,50 @@ teams_list = copy.deepcopy(constants.TEAMS)
 players_list = copy.deepcopy(constants.PLAYERS)
 not_experienced = []
 is_experienced = []
+panthers = []
+bandits = []
+warriors = []
 
 
 def clean_data():
     for player in players_list:
+        height = player["height"].split()
+        player["height"] = int(height[0])
+        player["guardians"] = player["guardians"].replace(" and ", ", ")
         if player["experience"] == "YES":
             player["experience"] = True
             is_experienced.append(player)
         elif player["experience"] == "NO":
             player["experience"] = False
             not_experienced.append(player)
-        height = player["height"].split()
-        player["height"] = int(height[0])
-        # Mel R {they/them} TeamTreeHouse Slack gave me a couple suggestions
-        # on how how to take the "and" out of the guardians list
-        player["guardians"] = player["guardians"].replace(" and ", ", ")
+    return not_experienced, is_experienced
 
 
 def balance_teams():
-    # I had previoulsy made this function so much more difficult than what I was trying to accomplish
-    # Mel R {they/them} suggested I look at my function and make sure it is doing everythig I'm intending it
-    # to do. That's when I realized the soution was much simpler. All I needed to do was use indexing to call
-    # a team and then use indexing again to put the first 3 exp players and the first 3 non_exp players on
-    # that team.
-    teams_list[0] = is_experienced[:3] + not_experienced[:3]
-    teams_list[1] = is_experienced[3:6] + not_experienced[3:6]
-    teams_list[2] = is_experienced[6:9] + not_experienced[6:9]
+    exp_players_per_team = int(len(is_experienced) / len(teams_list))
+    non_exp_players_per_team = int(len(not_experienced) / len(teams_list))
+    total_num_players_per_team = exp_players_per_team + non_exp_players_per_team
+    for player in is_experienced:
+        if len(bandits) < exp_players_per_team:
+            bandits.append(player)
+        elif len(panthers) < exp_players_per_team:
+            panthers.append(player)
+        elif len(warriors) < exp_players_per_team:
+            warriors.append(player)
+    for player in not_experienced:
+        if len(bandits) < total_num_players_per_team:
+            bandits.append(player)
+        elif len(panthers) < total_num_players_per_team:
+            panthers.append(player)
+        elif len(warriors) < total_num_players_per_team:
+            warriors.append(player)
+    return bandits, panthers, warriors
 
 
-def team_stat_menu():
+def team_select_menu():
+    team_1 = "Panthers"
+    team_2 = "Bandits"
+    team_3 = "Warriors"
     print("BASKETBALL TEAM STATS TOOL")
     print("\n")
     print("*********  MENU  *********")
@@ -58,147 +73,75 @@ Enter-> B) To Exit
             elif option.upper() == "B":
                 print("\nExiting Basketball Team Stats Tool.")
                 sys.exit()
-            # Jennifer Nordell TeamTreeHouse Slack was responsible for me being able to raise and except these
-            # ValueErrors correctly
-            raise ValueError()
-        except ValueError as e:
-            print("\nThat's not a valid option.Please try again.")
-            return team_stat_menu()
-    team_option = input("\nEnter an OPTION: ")
-    while True:
-        try:
-            if team_option.upper() == "A":
-                panthers = teams_list[0]
-                num_players = len(panthers)
-                # I rewatched the "Python Comprehensions Workshop" and realized This was the way to get the
-                # proper output for showing exp/non_exp players per team
-                exp_player = [player["experience"] for player in panthers if player["experience"] == True]
-                non_exp_player = [player["experience"] for player in panthers if player["experience"] == False]
-                height = [player["height"] for player in panthers]
-                # Got this solution from GeekForGeeks https://geeksforgeeks/find-average-list-python
-                average_height = round(sum(height) / len(panthers), 1)
-                print(
-                    f"\nTeam: Panthers Stats\n=+=+=+=+=+=+=+=+=+=+=+=\nPlayers: {int(num_players)} "
-                    f"Experienced Players: {int(len(exp_player))} "
-                    f"Inexperienced Players: {int(len(non_exp_player))} "
-                    f"Average Height on Team: {float(average_height)}\n")
-                team_a = []
-                for player in panthers:
-                    name = player["name"]
-                    team_a.append(str(name))
-                print("Players on Roster:")
-                # Mel R {they/them} TeamTreeHouse Slack helped me with this solution
-                print(", ".join(team_a))
-                print("\n")
-                team_a_guardians = []
-                for player in panthers:
-                    guardians = player["guardians"]
-                    team_a_guardians.append(str(guardians))
-                print("Players Guardians:")
-                # Mel R {they/them} TeamTreeHouse Slack helped me with this solution
-                print(", ".join(team_a_guardians))
-                more_stats = input("\nWould you like to see other teams? ENTER: Y or N ")
-                more_stats = str(more_stats)
-                if more_stats.upper() == "Y":
-                    print("\nOK, rerouting to the main menu.")
-                    return team_stat_menu()
-                elif more_stats.upper() == "N":
-                    print("\nExiting the Team Stats Tool.")
-                    sys.exit()
-                else:
-                    print("\nThat's not an OPTION. Rerouting to main menu.")
-                    return team_stat_menu()
-            elif team_option.upper() == "B":
-                bandits = teams_list[1]
-                num_players = len(bandits)
-                # I rewatched the "Python Comprehensions Workshop" and realized This was the way to get the
-                # proper output for showing exp/non_exp players per team
-                exp_player = [player["experience"] for player in bandits if player["experience"] == True]
-                non_exp_player = [player["experience"] for player in bandits if player["experience"] == False]
-                height = [player["height"] for player in bandits]
-                # got this solution from GeekForGeeks https://geeksforgeeks/find-average-list-python
-                average_height = round(sum(height) / len(bandits), 1)
-                print(
-                    f"\nTeam: Bandits Stats\n=+=+=+=+=+=+=+=+=+=+=+=\nPlayers: {int(num_players)} "
-                    f"Experienced Players: {int(len(exp_player))} "
-                    f"Inexperienced Players: {int(len(non_exp_player))} "
-                    f"Average Height on Team: {float(average_height)}\n")
-                team_b = []
-                for player in bandits:
-                    name = player["name"]
-                    team_b.append(str(name))
-                print("Players on Roster:")
-                # Mel R {they/them} TeamTreeHouse Slack helped me with this solution
-                print(", ".join(team_b))
-                print("\n")
-                team_b_guardians = []
-                for player in bandits:
-                    guardians = player["guardians"]
-                    team_b_guardians.append(str(guardians))
-                print("Players Guardians:")
-                # Mel R {they/them} TeamTreeHouse Slack helped me with this solution
-                print(", ".join(team_b_guardians))
-                more_stats = input("\n\nWould you like to see other teams? ENTER: Y or N ")
-                more_stats = str(more_stats)
-                if more_stats.upper() == "Y":
-                    print("\nOk rerouting to the main menu.")
-                    return team_stat_menu()
-                elif more_stats.upper() == "N":
-                    print("\nExiting the Team Stats Tool.")
-                    sys.exit()
-                else:
-                    print("\nThat's not an OPTION. Rerouting to main menu.")
-                    return team_stat_menu()
-            elif team_option.upper() == "C":
-                warriors = teams_list[2]
-                num_players = len(warriors)
-                # I rewatched the "Python Comprehensions Workshop" and realized This was the way to get the
-                # proper output for showing exp/non_exp players per team
-                exp_player = [player["experience"] for player in warriors if player["experience"] == True]
-                non_exp_player = [player["experience"] for player in warriors if player["experience"] == False]
-                height = [player["height"] for player in warriors]
-                # got this solution from GeekForGeeks https://geeksforgeeks/find-average-list-python
-                average_height = round(sum(height) / len(warriors), 1)
-                print(
-                    f"\nTeam: Warriors Stats\n=+=+=+=+=+=+=+=+=+=+=+=\nPlayers: {int(num_players)} "
-                    f"Experienced Players: {int(len(exp_player))} "
-                    f"Inexperienced Players: {int(len(non_exp_player))} "
-                    f"Average Height on Team: {float(average_height)}\n")
-                team_c = []
-                for player in warriors:
-                    name = player["name"]
-                    team_c.append(str(name))
-                print("Players on Roster:")
-                # Mel R {they/them} TeamTreeHouse Slack helped me with this solution
-                print(", ".join(team_c))
-                print("\n")
-                team_c_guardians = []
-                for player in warriors:
-                    guardians = player["guardians"]
-                    team_c_guardians.append(str(guardians))
-                print("Players Guardians:")
-                # Mel R {they/them} TeamTreeHouse Slack helped me with this solution
-                print(", ".join(team_c_guardians))
-                more_stats = input("\n\nWould you like to see other teams? ENTER: Y or N ")
-                more_stats = str(more_stats)
-                if more_stats.upper() == "Y":
-                    print("\nOk rerouting to the main menu.")
-                    return team_stat_menu()
-                elif more_stats.upper() == "N":
-                    print("\nExiting the Team Stats Tool.")
-                    sys.exit()
-                else:
-                    print("\nThat's not an OPTION. Rerouting to main menu.")
-                    return team_stat_menu()
-            # Jennifer Nordell TeamTreeHouse was responsible for me being able to raise and except these
-            # ValueErrors correctly
             raise ValueError()
         except ValueError as e:
             print("\nThat's not a valid option. Please try again.")
-            return team_stat_menu()
+            return team_select_menu()
+    while True:
+        try:
+            team_option = input("\nEnter an OPTION: ")
+            if team_option.upper() == "A":
+                print(f"\nTeam Name: {team_1}")
+                print("=+=+=+=+=+=+=+=+=+=+=+=")
+                players_exp_non_exp()
+                names_guardians_height(panthers)
+                see_other_teams()
+            elif team_option.upper() == "B":
+                print(f"\nTeam Name: {team_2}")
+                print("=+=+=+=+=+=+=+=+=+=+=+=")
+                players_exp_non_exp()
+                names_guardians_height(bandits)
+                see_other_teams()
+            elif team_option.upper() == "C":
+                print(f"\nTeam Name: {team_3}")
+                print("=+=+=+=+=+=+=+=+=+=+=+=")
+                players_exp_non_exp()
+                names_guardians_height(warriors)
+                see_other_teams()
+            raise ValueError()
+        except ValueError as e:
+            print("\nThat's not a valid option. Please try again.")
+        return team_select_menu()
+
+
+def players_exp_non_exp():
+    print(
+        f"Players on Team: {int(len(constants.PLAYERS) / len(constants.TEAMS))} | "
+        f"Experienced Players on Team: {int(len(is_experienced) / len(teams_list))} | "
+        f"Inexperienced Players on Team: {int(len(not_experienced)/ len(teams_list))} "
+        )
+
+
+def names_guardians_height(team_name):
+    names = [player["name"] for player in team_name]
+    guardians = []
+    height = [player["height"] for player in team_name]
+    for player in team_name:
+        player_guardian = player["guardians"]
+        guardians.append(player_guardian)
+    average_height = round(sum(height) / len(team_name), 1)
+    print(f"\nAverage Height on Team: {float(average_height)}")
+    print("\nNames of Players:")
+    print(", ".join(names))
+    print("\nGuardians of Players:")
+    print(", ".join(guardians))
+
+
+def see_other_teams():
+    more_stats = input("\nWould you like to see other teams? ENTER: Y or N ")
+    more_stats = str(more_stats)
+    if more_stats.upper() == "Y":
+        print("\nOK, rerouting to the main menu.")
+        return team_select_menu()
+    elif more_stats.upper() == "N":
+        print("\nExiting the Team Stats Tool.")
+        sys.exit()
+    else:
+        print("\nThat's not an OPTION. Rerouting to main menu.")
+        return team_select_menu()
 
 
 if __name__ == "__main__":
     clean_data()
     balance_teams()
-    team_stat_menu()
+    team_select_menu()
